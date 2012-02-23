78a12f57feb9f30a698a7c9a5e627a351ab899e8
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2012-02-23 09:34:04 +0100

    Showing preview upon upload

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,7 @@
 class Picture < ActiveRecord::Base
   has_attached_file :file, :styles => { :medium => "300x300>", :thumb => "50x50>" }
+
+  before_save do |pic|
+    pic.name = pic.file.original_filename if pic.name.blank?
+  end
 end
